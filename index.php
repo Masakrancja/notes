7064e97cfae085b9c6fd057e3adff6a4bc1e89c7
piotr <piotr@piotr.local>
2023-04-17 19:51:05 +0200

    2023-04-17 19:51

--- index.php
+++ index.php
@@ -5,35 +5,12 @@
 
   $action = null;
   if (!empty($_GET['action'])) {
-    $action = $_GET['action'];
+    $action = htmlentities($_GET['action']);
   }
-?>
 
-<html>
-  <head>
-
-  </head>
-  <body>
-    <div class="header">
-      <h1>Nagłówek</h1>
-    </div>
-
-    <div class="container">
-      <div class="menu">
-        <ul>
-          <li><a href="/">Lista notatek</a></li>
-          <li><a href="/?action=create">Utwórz notatkę</a></li>
-          <li></li>
-
-        </ul>
-      </div>
-      <div class="content"></div>
-    </div>
-
-    <div class="footer">
-      Stopka
-    </div>
-
-  </body>
-</html>
+  if ($action === 'create') {
+    include_once('templates/pages/create.php');
+  } else {
+    include_once('templates/pages/list.php');
+  }
 
---
+++ templates/pages/create.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+
+  </head>
+  <body>
+    <div class="header">
+      <h1>Nagłówek</h1>
+    </div>
+
+    <div class="container">
+      <div class="menu">
+        <ul>
+          <li><a href="/">Lista notatek</a></li>
+          <li><a href="/?action=create">Utwórz notatkę</a></li>
+          <li></li>
+
+        </ul>
+      </div>
+      <div class="content">
+        <h2>Utwórz</h2>
+
+
+
+      </div>
+    </div>
+
+    <div class="footer">
+      Stopka
+    </div>
+
+  </body>
+</html>
+
